e4d451ec630e76ea7891c04520b39e46a28079a8
Aaron Bieber <aaron@aaronbieber.com>
2013-03-12 17:14:40 -0400

    Assume dmenu with Xft support.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,23 +2,21 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import System.IO
-
---	, focusedBorderColor = "#87db00"
---	, normalBorderColor = "#cccccc"
 
 main = do
 xmproc <- spawnPipe "/usr/bin/xmobar /home/airborne/.xmobarrc"
-xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
+xmonad $ defaultConfig { 
+	  manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
 	, borderWidth = 2
 	, focusedBorderColor = "#ffffff"
 	, normalBorderColor = "#444444"
 	, focusFollowsMouse = False
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") --mod4mask is the windows key
-    , ((0, xK_Print), spawn "gnome-screenshot")
-    ]
-
+    }
+	`additionalKeysP`
+	[ ("C-=", spawn "backlight-adjust +")
+	, ("C--", spawn "backlight-adjust -")
+	, ("M-p", spawn "dmenu_run -i -nb '#282b57' -nf '#eeeeff' -sb '#555a9e' -fn 'Inconsolata-10'")
+	]
