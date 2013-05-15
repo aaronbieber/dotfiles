d0662273a9fa4a7df001ace3dcfbd40ef0d9e5b7
Aaron Bieber <aaron@aaronbieber.com>
2013-05-15 08:25:28 -0400

    Removed xmobar for VirtualBox and indented config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,22 +1,34 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Actions.CycleWS
 import System.IO
 
 main = do
-xmproc <- spawnPipe "/usr/bin/xmobar /home/airborne/.xmobarrc"
-xmonad $ defaultConfig { 
-	  manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-	, borderWidth = 2
-	, focusedBorderColor = "#ffffff"
-	, normalBorderColor = "#444444"
-	, focusFollowsMouse = False
-    }
-	`additionalKeysP`
-	[ ("C-=", spawn "backlight-adjust +")
-	, ("C--", spawn "backlight-adjust -")
-	, ("M-p", spawn "dmenu_run -i -nb '#282b57' -nf '#eeeeff' -sb '#555a9e' -fn 'Inconsolata-10'")
-	]
+    --xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+    xmonad $ defaultConfig { 
+          manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , borderWidth = 2
+        , focusedBorderColor = "#ffffff"
+        , normalBorderColor = "#444444"
+        , focusFollowsMouse = False
+		, terminal = "urxvt"
+        }
+        `additionalKeysP` (
+        [ ("C-=", spawn "backlight-adjust +")
+        , ("C--", spawn "backlight-adjust -")
+        , ("M-p", spawn "dmenu_run -i -nb '#282b57' -nf '#eeeeff' -sb '#555a9e' -fn 'Inconsolata-10'")
+        ]
+        -- ++ [
+        --      (otherModMasks ++ "M-" ++ [key], action tag)
+        --    | (tag, key) <- zip (XMonad.workspaces defaultConfig) "123456789"
+        --    , (otherModMasks, action) < - [ ("", windows . W.view)
+        --                                  , ("S-", windows . W.shift) ]
+        --]
+        )
