b23a39f091cca918cc3d3b587e7c18d616ed0a98
Aaron Bieber <aaron@aaronbieber.com>
2015-01-09 11:25:45 -0500

    New binding for displaying current track.

--- configs/xmonad/xmonad.hs
+++ configs/xmonad/xmonad.hs
@@ -26,6 +26,7 @@ main = do
         , ("S-M-l", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
         , ("S-M-h", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
         , ("S-M-p", spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+        , ("S-M-o", spawn "notify-send -t 1500 'Current Track' \"$(show_track)\"")
         , ("S-M-j", spawn "amixer -q sset Master 5%-")
         , ("S-M-k", spawn "amixer -q sset Master 5%+")
         , ("M-p", spawn "dmenu_run -i -nb '#282b57' -nf '#eeeeff' -sb '#555a9e' -fn 'Inconsolata-10'")
