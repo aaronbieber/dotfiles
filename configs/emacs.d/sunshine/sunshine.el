aa95810f95d0e9b4e8939cccae73f37b930d30f4
Aaron Bieber <aaron@aaronbieber.com>
2015-01-19 12:06:32 -0500

    Stub for a Sunshine module for Emacs.

---
+++ configs/emacs.d/sunshine/sunshine.el
@@ -0,0 +1,9 @@
+;;; sunshine.el --- Provide weather and forecast information.
+;;; Commentary:
+;;
+;; Use OpenWeatherMap's API to provide current weather and forecast information within Emacs!
+;;
+;;; Code:
+
+(provide 'sunshine)
+;;; sunshine.el ends here
