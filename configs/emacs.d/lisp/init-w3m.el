99aa669a7193787bb3434857e8618d8cc0c32bbb
Aaron Bieber <aaron@aaronbieber.com>
2015-02-10 13:30:51 -0500

    Half-screen scroll mappings for w3m.

--- configs/emacs.d/lisp/init-w3m.el
+++ configs/emacs.d/lisp/init-w3m.el
@@ -1,6 +1,13 @@
 ;;; For all of your web browsing needs.
 (when (maybe-require-package 'w3m)
       (setq browse-url-browser-function 'w3m-goto-url-new-session)
-      (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533."))
+      (setq w3m-user-agent (concat "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; "
+                                   "HTC_Pyramid Build/GRI40) "
+                                   "AppleWebKit/533.1 (KHTML, like Gecko) "
+                                   "Version/4.0 Mobile Safari/533."))
+
+      (add-hook 'w3m-mode-hook (lambda ()
+                (define-key w3m-mode-map (kbd "d") (lambda () (interactive) (w3m-scroll-up (/ (window-height) 2))))
+                (define-key w3m-mode-map (kbd "u") (lambda () (interactive) (w3m-scroll-down (/ (window-height) 2))))))
 
 (provide 'init-w3m)
