ed2ed3615330071cfa474f259ec4d453ffceab0d
Aaron Bieber <aaron@aaronbieber.com>
2015-05-04 08:26:54 -0400

    Make TODO maps pop to window.

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -1,7 +1,11 @@
 ;;; Provide global maps that aren't specific to any mode or package.
 
-(define-key global-map (kbd "C-c t w") (lambda () (interactive) (find-file "~/Dropbox/org/work.org")))
-(define-key global-map (kbd "C-c t h") (lambda () (interactive) (find-file "~/Dropbox/org/home.org")))
+(define-key global-map (kbd "C-c t w") (lambda ()
+                                         (interactive)
+                                         (find-file-other-window "~/Dropbox/org/work.org")))
+(define-key global-map (kbd "C-c t h") (lambda ()
+                                         (interactive)
+                                         (find-file-other-window "~/Dropbox/org/home.org")))
 (define-key global-map (kbd "C-x C-q") 'kill-emacs)
 (define-key global-map (kbd "C-c C-u") 'insert-char) ;; "u" for Unicode, get it?
 (define-key global-map (kbd "C-c l") 'dictionary-lookup-definition)
