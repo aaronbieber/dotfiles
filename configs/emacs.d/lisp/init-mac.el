e015263482aaa1b6a73aec458b651b4fb8b412a3
Aaron Bieber <aaron@aaronbieber.com>
2015-05-16 08:08:06 -0400

    Super-RET to toggle fullscreen on Mac.

--- configs/emacs.d/lisp/init-mac.el
+++ configs/emacs.d/lisp/init-mac.el
@@ -3,7 +3,6 @@
 (when (memq window-system '(mac ns))
   (add-to-list 'default-frame-alist '(font . "Menlo"))
   (set-face-attribute 'default t :font "Menlo")
-  (define-key global-map (kbd "<S-return>") 'toggle-frame-fullscreen)
-  (define-key global-map (kbd "<M-S-return>") 'toggle-frame-fullscreen))
+  (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
 (provide 'init-mac)
