5ed47831da14c6ee8fd3c85734170ba66da1267c
Aaron Bieber <aaron@aaronbieber.com>
2015-05-16 08:06:59 -0400

    Documentation, alignment.

--- configs/emacs.d/lisp/init-evil.el
+++ configs/emacs.d/lisp/init-evil.el
@@ -2,26 +2,25 @@
   ;; Evil leader must be loaded before evil (as documented).
   (global-evil-leader-mode)
 
-  ;; Configure evil-leader
   (evil-leader/set-leader ",")
   (setq evil-leader/in-all-states 1)
   (evil-leader/set-key
     "."  'switch-to-previous-buffer
     "aa" 'align-regexp
     "a=" 'my-align-single-equals
-    "b"  'helm-mini
+    "b"  'helm-mini             ;; Switch to another buffer
     "B"  'magit-blame-mode
     "d"  (lambda () (interactive) (evil-ex-call-command nil "bdelete" nil))
     "D"  'open-current-line-in-codebase-search
-    "f"  'helm-imenu
+    "f"  'helm-imenu            ;; Jump to function in buffer
     "g"  'magit-status
-    "h"  'fontify-and-browse
-    "l"  'whitespace-mode
-    "nn" 'narrow-and-set-normal ;; "narrow normal"
-    "nw" 'widen                 ;; "narrow widen"
-    "o"  'delete-other-windows
+    "h"  'fontify-and-browse    ;; HTML-ize the buffer and browse the result
+    "l"  'whitespace-mode       ;; Show invisible characters
+    "nn" 'narrow-and-set-normal ;; Narrow to region and enter normal mode
+    "nw" 'widen
+    "o"  'delete-other-windows  ;; C-w o
     "p"  'put-from-x-clipboard
-    "s"  'ag-project            ;; "search"
+    "s"  'ag-project            ;; Ag search from project's root
     "r"  (lambda () (interactive) (font-lock-fontify-buffer) (redraw-display))
     "S"  'delete-trailing-whitespace
     "t"  'gtags-reindex
@@ -30,7 +29,8 @@
     "x"  'helm-M-x
     "y"  'yank-to-x-clipboard))
 
-(maybe-require-package 'evil-jumper)
+(when (maybe-require-package 'evil-jumper)
+  (global-evil-jumper-mode))
 (maybe-require-package 'evil-surround)
 (maybe-require-package 'evil-indent-textobject)
 
@@ -78,14 +78,14 @@
 
   ;; Global bindings.
   (define-key evil-normal-state-map (kbd "C-S-P") 'helm-projectile-switch-project)
-  (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile)
-  (define-key evil-normal-state-map (kbd "-")   'helm-find-files)
-  (define-key evil-normal-state-map (kbd "C-]") 'gtags-find-tag-from-here)
-  (define-key evil-normal-state-map (kbd "g/")  'occur-last-search)
-  (define-key evil-normal-state-map (kbd "[i")  'show-first-occurrence)
-  (define-key evil-normal-state-map (kbd "]n") 'next-conflict-marker)
-  (define-key evil-normal-state-map (kbd "[n") 'previous-conflict-marker)
-  (define-key evil-insert-state-map (kbd "C-e") 'end-of-line)
+  (define-key evil-normal-state-map (kbd "C-p")   'helm-projectile)
+  (define-key evil-normal-state-map (kbd "-")     'helm-find-files)
+  (define-key evil-normal-state-map (kbd "C-]")   'gtags-find-tag-from-here)
+  (define-key evil-normal-state-map (kbd "g/")    'occur-last-search)
+  (define-key evil-normal-state-map (kbd "[i")    'show-first-occurrence)
+  (define-key evil-normal-state-map (kbd "]n")    'next-conflict-marker)
+  (define-key evil-normal-state-map (kbd "[n")    'previous-conflict-marker)
+  (define-key evil-insert-state-map (kbd "C-e")   'end-of-line)
 
   (defun minibuffer-keyboard-quit ()
     "Abort recursive edit.
