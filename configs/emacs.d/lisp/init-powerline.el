5b84fdf680a9c3c32cf6f06e829a5d46fdababfa
Aaron Bieber <aaron@aaronbieber.com>
2015-05-17 12:58:29 -0400

    Amazing new Powerline format.

--- configs/emacs.d/lisp/init-powerline.el
+++ configs/emacs.d/lisp/init-powerline.el
@@ -1,42 +1,39 @@
 (when (and (maybe-require-package 'powerline)
            (maybe-require-package 'powerline-evil))
 
-  (setq powerline-default-separator 'arrow)
+  (setq powerline-default-separator 'arrow-fade)
 
-  (defface my-mode-line-active
-    '((t (:foreground "#B9EDD6" :background "#105C3A")))
-    "Active mode line face (left and right edges).")
+  (defface my-pl-segment1-active
+    '((t (:foreground "black" :background "#E1B61A")))
+    "Powerline first segment active face.")
+  (defface my-pl-segment1-inactive
+    '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+    "Powerline first segment inactive face.")
+  
+  (defface my-pl-segment2-active
+    '((t (:foreground "#F5E39F" :background "#8A7119")))
+    "Powerline second segment active face.")
+  (defface my-pl-segment2-inactive
+    '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+    "Powerline second segment inactive face.")
 
-  (defface my-mode-line-inactive
-    '((t (:foreground "#11613D" :background "#082E1D")))
-    "Inactive mode line face (left and right edges).")
-
-  (defface my-powerline-active1
-    '((t (:foreground "#B9EDD6" :background "#0C422A")))
-    "Active primary powerline face.")
-
-  (defface my-powerline-active2
-    '((t (:foreground "#B9EDD6" :background "#082E1D")))
-    "Active secondary powerline face.")
-
-  (defface my-powerline-inactive1
-    '((t (:foreground "#11613D" :background "#082E1D")))
-    "Inactive primary powerline face.")
-
-  (defface my-powerline-inactive2
-    '((t (:foreground "#11613D" :background "#082E1D")))
-    "Inactive secondary powerline face.")
+  (defface my-pl-segment3-active
+    '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+    "Powerline third segment active face.")
+  (defface my-pl-segment3-inactive
+    '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+    "Powerline third segment inactive face.")
 
   (defun my-powerline-default-theme ()
-    "Setup the default mode-line."
+    "Set up my custom Powerline with Evil indicators."
     (interactive)
     (setq-default mode-line-format
                   '("%e"
                     (:eval
                      (let* ((active (powerline-selected-window-active))
-                            (mode-line (if active 'my-mode-line-active 'my-mode-line-inactive))
-                            (face1 (if active 'my-powerline-active1 'my-powerline-inactive1))
-                            (face2 (if active 'my-powerline-active2 'my-powerline-inactive2))
+                            (seg1 (if active 'my-pl-segment1-active 'my-pl-segment1-inactive))
+                            (seg2 (if active 'my-pl-segment2-active 'my-pl-segment2-inactive))
+                            (seg3 (if active 'my-pl-segment3-active 'my-pl-segment3-inactive))
                             (separator-left (intern (format "powerline-%s-%s"
                                                             (powerline-current-separator)
                                                             (car powerline-default-separator-dir))))
@@ -48,42 +45,42 @@
                                               (powerline-raw (powerline-evil-tag) evil-face)
                                               ))
                                        (if evil-mode
-                                           (funcall separator-left (powerline-evil-face) mode-line))
+                                           (funcall separator-left (powerline-evil-face) seg1))
                                        ;;(powerline-raw "%*" nil 'l)
                                        ;;(when powerline-display-buffer-size
                                        ;;  (powerline-buffer-size nil 'l))
                                        ;;(when powerline-display-mule-info
                                        ;;  (powerline-raw mode-line-mule-info nil 'l))
-                                       (powerline-buffer-id mode-line 'l)
+                                       (powerline-buffer-id seg1 'l)
                                        (when (and (boundp 'which-func-mode) which-func-mode)
-                                         (powerline-raw which-func-format mode-line 'l))
-                                       (powerline-raw " " mode-line)
-                                       (funcall separator-left mode-line face1)
+                                         (powerline-raw which-func-format seg1 'l))
+                                       (powerline-raw " " seg1)
+                                       (funcall separator-left seg1 seg2)
                                        (when (boundp 'erc-modified-channels-object)
-                                         (powerline-raw erc-modified-channels-object face1 'l))
-                                       (powerline-major-mode face1 'l)
-                                       (powerline-process face1)
-                                       (powerline-minor-modes face1 'l)
-                                       (powerline-narrow face1 'l)
-                                       (powerline-raw " " face1)
-                                       (funcall separator-left face1 face2)
-                                       (powerline-vc face2 'r)
+                                         (powerline-raw erc-modified-channels-object seg2 'l))
+                                       (powerline-major-mode seg2 'l)
+                                       (powerline-process seg2)
+                                       (powerline-minor-modes seg2 'l)
+                                       (powerline-narrow seg2 'l)
+                                       (powerline-raw " " seg2)
+                                       (funcall separator-left seg2 seg3)
+                                       (powerline-vc seg3 'r)
                                        (when (bound-and-true-p nyan-mode)
-                                         (powerline-raw (list (nyan-create)) face2 'l))))
-                            (rhs (list (powerline-raw global-mode-string face2 'r)
-                                       (funcall separator-right face2 face1)
+                                         (powerline-raw (list (nyan-create)) seg3 'l))))
+                            (rhs (list (powerline-raw global-mode-string seg3 'r)
+                                       (funcall separator-right seg3 seg2)
                                        (unless window-system
-                                         (powerline-raw (char-to-string #xe0a1) face1 'l))
-                                       (powerline-raw "%4l" face1 'l)
-                                       (powerline-raw ":" face1 'l)
-                                       (powerline-raw "%3c" face1 'r)
-                                       (funcall separator-right face1 mode-line)
-                                       (powerline-raw " " mode-line)
-                                       (powerline-raw "%6p" mode-line 'r)
+                                         (powerline-raw (char-to-string #xe0a1) seg2 'l))
+                                       (powerline-raw "%4l" seg2 'l)
+                                       (powerline-raw ":" seg2 'l)
+                                       (powerline-raw "%3c" seg2 'r)
+                                       (funcall separator-right seg2 seg1)
+                                       (powerline-raw " " seg1)
+                                       (powerline-raw "%6p" seg1 'r)
                                        (when powerline-display-hud
-                                         (powerline-hud face2 mode-line)))))
+                                         (powerline-hud seg1 seg3)))))
                        (concat (powerline-render lhs)
-                               (powerline-fill face2 (powerline-width rhs))
+                               (powerline-fill seg3 (powerline-width rhs))
                                (powerline-render rhs))))))))
   
 (provide 'init-powerline)
