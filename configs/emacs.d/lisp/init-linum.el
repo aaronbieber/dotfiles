6f23e7f620619d6872f9f4e5bee8b933f1fe2841
Aaron Bieber <aaron@aaronbieber.com>
2015-05-21 14:37:09 -0400

    Refined relative line numbering format.

--- configs/emacs.d/lisp/init-linum.el
+++ configs/emacs.d/lisp/init-linum.el
@@ -1,21 +1,28 @@
 ;;; Stuff for line numbers.
+(defface linum-current
+  '((t (:inherit linum :weight bold :underline "#555")))
+  "The current line number.")
+
 (defun my-linum-get-format-string ()
   (let* ((width (max 4 (1+ (length (number-to-string
                              (count-lines (point-min) (point-max)))))))
-         (format (concat "%" (number-to-string width) "d ")))
-    (setq my-linum-format-string format)))
+         (format (concat "%" (number-to-string width) "d "))
+         (current-line-format (concat "%-" (number-to-string width) "d ")))
+    (setq my-linum-format-string format)
+	(setq my-linum-current-line-format-string current-line-format)))
 
 (defvar my-linum-current-line-number 0)
 
 (setq linum-format 'my-linum-relative-line-numbers)
 
 (defun my-linum-relative-line-numbers (line-number)
-  (let* ( (offset (abs (- line-number my-linum-current-line-number)))
-          (linum-display-value (if (= 0 offset)
-				   my-linum-current-line-number
-				   offset))
-        )
-    (propertize (format my-linum-format-string linum-display-value) 'face 'linum)))
+  (let* ((offset (abs (- line-number my-linum-current-line-number)))
+         (linum-display-value (if (= 0 offset)
+			   my-linum-current-line-number
+                                offset))
+         (format-string (if (= my-linum-current-line-number line-number) my-linum-current-line-format-string my-linum-format-string))
+         (face (if (= my-linum-current-line-number line-number) 'linum-current 'linum)))
+    (propertize (format format-string linum-display-value) 'face face)))
 
 (defadvice linum-update (around my-linum-update)
   (let ((my-linum-current-line-number (line-number-at-pos)))
