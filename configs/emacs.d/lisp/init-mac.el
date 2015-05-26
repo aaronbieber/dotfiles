d06d1ba9725b22afa52337e41ed60e0de2c0ad9a
Aaron Bieber <aaron@aaronbieber.com>
2015-05-26 19:13:32 -0400

    Mac update, package config update.

--- configs/emacs.d/lisp/init-mac.el
+++ configs/emacs.d/lisp/init-mac.el
@@ -2,8 +2,8 @@
 (require 'init-fonts)
 
 (when (memq window-system '(mac ns))
-  (add-to-list 'default-frame-alist '(font . "Menlo"))
-  (set-face-attribute 'default t :font "Menlo")
+  (add-to-list 'default-frame-alist '(font . "Menlo-14"))
+  (set-face-attribute 'default t :font "Menlo-14")
   (sanityinc/set-frame-font-size 14)
   (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
