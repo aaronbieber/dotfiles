4b87bdc22834ef0cff30cb69313b8cdd3fc3876b
Aaron Bieber <aaron@aaronbieber.com>
2015-05-31 12:23:29 -0400

    Working server and thing creation.

--- configs/emacs.d/octopress-mode/octopress-mode.el
+++ configs/emacs.d/octopress-mode/octopress-mode.el
@@ -12,12 +12,10 @@
 
 (defvar octopress-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "q" 'om-quit)
-    (define-key map "s" 'om-start-server)
-    (define-key map "k" 'om-stop-server)
+    (define-key map "q" 'om-status-quit)
+    (define-key map "s" 'om-start-stop-server)
     (define-key map "g" 'om-refresh-status)
-    (define-key map "N" 'om-new-post)
-    (define-key map "n" 'om-new-draft)
+    (define-key map "n" 'om-new-thing)
     (define-key map "d" 'om-deploy)
     (define-key map "b" 'om-build)
     (define-key map "$" 'om-show-server)
@@ -43,32 +41,68 @@
   :type 'string
   :group 'octopress-mode)
 
+;;(defcustom octopress-command-new-post)
+;;(defcustom octopress-command-new-draft)
+;;(defcustom octopress-command-build)
+;;(defcustom octopress-command-deploy)
+
 (defun om-refresh-status ()
-  (if (eq (current-buffer) (get-buffer (om--buffer-name-for-type "status")))
-      (om--draw-status (current-buffer))))
-
-(defun om-start-server (&optional with-drafts)
   (interactive)
-  (om--start-server-process))
+  (om--maybe-redraw-status))
 
-(defun om-stop-server ()
+(defun om-start-stop-server (&optional with-drafts)
   (interactive)
-  (om--stop-server-process))
+  (let ((choice (read-char-choice
+                 "[s] Start, [d] Start w/drafts, [k] Kill, [q] Abort"
+                 '(?s ?d ?k ?q))))
+    (cond ((eq choice ?s)
+           (progn (message "Starting server normally...")
+                  (om--start-server-process)))
+          ((eq choice ?d)
+           (progn (message "Starting server with drafts...")
+                  (om--start-server-process t)))
+          ((eq choice ?k)
+           (progn (message "Stopping server...")
+                  (om--stop-server-process)))
+          ((eq choice ?q)
+           (message "Aborted.")))))
 
 (defun om-restart-server ()
   (interactive))
 
-(defun om-new-post ()
-  (interactive))
+(defun om-show-server ()
+  (interactive)
+  (pop-to-buffer (get-buffer (om--buffer-name-for-type "server"))))
 
-(defun om-new-draft ()
-  (interactive))
+(defun om-new-thing ()
+  (interactive)
+  (let ((choice (read-char-choice
+                 "[p] Post, [d] Draft, [g] Page, [q] Abort"
+                 '(?p ?d ?g ?q))))
+    (cond ((eq choice ?p)
+           (om--new-post))
+          ((eq choice ?d)
+           (om--new-draft))
+          ((eq choice ?g)
+           (om--new-page))
+          ((eq choice ?q)
+           (message "Aborted.")))))
 
 (defun om-deploy ()
   (interactive))
 
 (defun om-build (&optional with-drafts)
   (interactive))
+
+(defun om-status-quit ()
+  "Quit the Octopress Mode window, preserving its buffer."
+  (interactive)
+  (quit-window))
+
+(defun om-server-quit ()
+  "Quit the Octopress Server Mode window, preserving its buffer."
+  (interactive)
+  (quit-window))
 
 ;;; "Private" -- not meant to be used by users.
 (defun om--setup ()
@@ -77,14 +111,26 @@
   (let ((om-buffer (get-buffer (om--buffer-name-for-type "status"))))
     (if (om--buffer-is-configured om-buffer)
         om-buffer
-      (let* ((om-buffer (om--prepare-status-buffer))
-             (om-root (om--get-root)))
+      (setq om-root (om--get-root))
+      (let* ((om-buffer (om--prepare-status-buffer)))
         (if (and om-buffer om-root)
             (progn (with-current-buffer om-buffer
-                     (set (make-local-variable 'om-root) om-root))
+                     (make-local-variable 'om-root))
                 om-buffer)
           (progn (kill-buffer om-buffer)
                  nil))))))
+
+(defun om--new-post ()
+  (let ((name (read-string "Post name: ")))
+    (om--run-octopress-command (concat "octopress new post \"" name "\""))))
+
+(defun om--new-draft ()
+  (let ((name (read-string "Draft name: ")))
+    (om--run-octopress-command (concat "octopress new draft \"" name "\""))))
+
+(defun om--new-page ()
+  (let ((name (read-string "Page name: ")))
+    (om--run-octopress-command (concat "octopress new page \"" name "\""))))
 
 (defun om--buffer-is-configured (buffer)
   "Return t if BUFFER is configured properly for Octopress Mode."
@@ -94,18 +140,20 @@
          (and (assoc 'om-root vars)
               (string= (cdr (assoc 'major-mode vars)) "octopress-mode")))))
 
-(defun om--start-server-process ()
-  (let ((om-server-process-buffer (om--prepare-server-buffer)))
+(defun om--start-server-process (&optional with-drafts)
+  (om--setup)
+  (let ((om-server-process-buffer (om--prepare-server-buffer))
+        (drafts-opt (if with-drafts " -D" nil)))
     (if (processp (get-buffer-process om-server-process-buffer))
-        (message "Server already running!"))
-    (setq om-server-process
-          (start-process-shell-command
-           "octopress-server"
-           om-server-process-buffer
-           "cd ~/Blog && jekyll serve"))
-    (message "Server started!")
-    (set-process-sentinel om-server-process 'om--process-sentinel)
-    (om--maybe-redraw-status)))
+        (message "Server already running!")
+      (setq om-server-process
+            (start-process-shell-command
+             "octopress-server"
+             om-server-process-buffer
+             (concat "cd " (om--get-root) " && jekyll serve" drafts-opt)))
+      (message "Server started!")
+      (set-process-sentinel om-server-process 'om--server-sentinel)
+      (om--maybe-redraw-status))))
 
 (defun om--stop-server-process ()
   (let ((server-process (get-buffer-process (om--buffer-name-for-type "server"))))
@@ -116,9 +164,15 @@
   "Return a buffer name for the provided type."
   (concat "*om-" type "*"))
 
-(defun om--process-sentinel (process event)
+(defun om--server-sentinel (process event)
   (om--maybe-redraw-status)
-  (message "Process `%s' had event `%s'." process event))
+  (let ((program (process-name process))
+        (event (replace-regexp-in-string "\n$" "" event)))
+    (message (concat program " - " event)) ;; @todo @delete
+    (cond ((string-prefix-p "finished" event)
+           (progn (message "Octopress server has stopped.")
+                  (with-current-buffer (om--prepare-server-buffer)
+                    (insert "\nServer process ended.")))))))
 
 (defun om--server-status ()
   (let ((server-process (get-buffer-process (om--buffer-name-for-type "server"))))
@@ -130,31 +184,30 @@
       "Running"
     "Stopped"))
 
+(defun om--prepare-buffer-for-type (type &optional mode-function)
+  "Prepare an empty buffer for TYPE and optionally run MODE-FUNCTION."
+  (let ((buffer-name (om--buffer-name-for-type type)))
+    (if (bufferp buffer-name)
+        (get-buffer buffer-name)
+      (let ((buf (get-buffer-create buffer-name)))
+        (with-current-buffer buf
+          (setq buffer-read-only nil)
+          (kill-all-local-variables)
+          (if (functionp mode-function)
+              (funcall mode-function)))
+        buf))))
+
 (defun om--prepare-status-buffer ()
   "Create an empty, writable, Octopress Mode buffer and return it."
-  (let ((buf (get-buffer-create (om--buffer-name-for-type "status"))))
-    (with-current-buffer buf
-      (setq buffer-read-only nil)
-      (erase-buffer)
-      (kill-all-local-variables)
-      (octopress-mode))
-    buf))
+  (om--prepare-buffer-for-type "status" 'octopress-mode))
 
 (defun om--prepare-server-buffer ()
   "Create an empty, writable, Octopress Server Mode buffer and return it."
-  (let ((buf (get-buffer-create (om--buffer-name-for-type "server"))))
-    (if (not (processp (get-buffer-process buf)))
-             (with-current-buffer buf
-               (setq buffer-read-only nil)
-               (erase-buffer)
-               (kill-all-local-variables)
-               (octopress-server-mode)))
-        buf))
+  (om--prepare-buffer-for-type "server" 'octopress-server-mode))
 
-(defun om-quit ()
-  "Quit the Octopress Mode window, preserving its buffer."
-  (interactive)
-  (quit-window))
+(defun om--prepare-process-buffer ()
+  "Create an empty, writable, Octopress Process Mode buffer and return it."
+  (om--prepare-buffer-for-type "process" 'octopress-process-mode))
 
 (defun om--get-root ()
   "Attempt to find the root of the Octopress site.
@@ -162,19 +215,25 @@
 We assume we are running from a buffer editing a file somewhere within the site.
 If we are running from some other kind of buffer, or a buffer with no file, the
 user will be prompted to enter the path to an Octopress site."
-  (let* ((this-dir (if (and (boundp 'dired-directory) dired-directory)
+  (let ((status-buffer (get-buffer (om--buffer-name-for-type "status")))
+        (this-dir (if (and (boundp 'dired-directory) dired-directory)
                       dired-directory
                     (if (buffer-file-name (current-buffer))
                         (file-name-directory (buffer-file-name (current-buffer)))))))
-    (or (and this-dir
-             (expand-file-name (vc-find-root this-dir "_config.yml")))
-        (let ((candidate-dir (read-directory-name "Octopress site root: ")))
-          (if (file-exists-p (expand-file-name "_config.yml" candidate-dir))
-              (expand-file-name candidate-dir)
-            (prog2 (message "Could not find _config.yml in `%s'." candidate-dir)
-                nil))))))
+    (if (and (bufferp status-buffer)
+             (assoc 'om-root (buffer-local-variables status-buffer))
+             (buffer-local-value 'om-root status-buffer))
+        (buffer-local-value 'om-root status-buffer)
+      (or (and this-dir
+               (expand-file-name (vc-find-root this-dir "_config.yml")))
+          (let ((candidate-dir (read-directory-name "Octopress site root: ")))
+            (if (file-exists-p (expand-file-name "_config.yml" candidate-dir))
+                (expand-file-name candidate-dir)
+              (prog2 (message "Could not find _config.yml in `%s'." candidate-dir)
+                  nil)))))))
 
 (defun om--maybe-redraw-status ()
+  "If the status buffer exists, redraw it with current information."
   (let ((status-buffer (get-buffer (om--buffer-name-for-type "status"))))
     (if (bufferp status-buffer)
         (om--draw-status status-buffer))))
@@ -207,15 +266,60 @@ STATUS is an alist of status names and their printable values."
     (with-current-buffer buffer
       (setq buffer-read-only nil)
       (erase-buffer)
-      (insert "Octopress Status\n"
-              "\n"
-              "    Blog root: " om-root "\n"
-              "       Server: " (cdr (assoc 'server-status status)) "\n"
-              "       Drafts: " (cdr (assoc 'drafts-count status)) "\n"
-              "        Posts: " (cdr (assoc 'posts-count status)))
+      (insert
+       (propertize "Octopress Status\n" 'face 'font-lock-constant-face)
+       "\n"
+       "    Blog root: " om-root "\n"
+       "       Server: " (cdr (assoc 'server-status status)) "\n"
+       "       Drafts: " (cdr (assoc 'drafts-count status)) "\n"
+       "        Posts: " (cdr (assoc 'posts-count status)) "\n"
+       "\n"
+       (propertize "Commands:\n" 'face 'font-lock-constant-face)
+       (propertize "n" 'face 'font-lock-keyword-face) ": New"     (make-string 4 ? )
+       (propertize "s" 'face 'font-lock-keyword-face) ": Server"  (make-string 4 ? )
+       (propertize "g" 'face 'font-lock-keyword-face) ": Refresh" (make-string 4 ? )
+       (propertize "d" 'face 'font-lock-keyword-face) ": Deploy"  (make-string 4 ? )
+       (propertize "b" 'face 'font-lock-keyword-face) ": Build"   (make-string 4 ? )
+       (propertize "q" 'face 'font-lock-keyword-face) ": Quit\n")
       (goto-char (point-min))
       (setq buffer-read-only t))))
 
+(defun om--run-octopress-command (command)
+  "Run an Octopress command, sending output to the process buffer.
+
+Returns the process object."
+  (om--setup)
+  (let ((pbuffer (om--prepare-process-buffer))
+        (om-root (om--get-root))
+        (command (replace-regexp-in-string "'" "\\\\'" command)))
+    (message "Running `%s'..." command)
+    (with-current-buffer pbuffer
+      (goto-char (point-max)))
+    (let ((process (start-process-shell-command
+                    "octopress"
+                    pbuffer
+                    (concat "cd " om-root " && " command))))
+      (set-process-sentinel process 'om--octopress-sentinel)
+      process)))
+
+(defun om--octopress-sentinel (process event)
+  (let ((program (process-name process))
+        (event (replace-regexp-in-string "\n$" "" event)))
+    (message (concat program " - " event)) ;; @todo @delete
+    (cond ((string-prefix-p "finished" event)
+           (progn (with-current-buffer (om--buffer-name-for-type "process")
+                    (insert "\n-\n\n"))
+                  (message "Octopress has completed.")))
+          ((string-prefix-p "exited" event)
+           (message "Octopress exited abnormally; check the process output for information.")))))
+
+(defun om--prop-command (key label)
+  "Propertize a command legend item with pretty colors.
+
+Return a propertized string like KEY: LABEL."
+  (concat (propertize key 'face 'font-lock-keyword-face) ": " label))
+
+;;;###autoload
 (defun om-status ()
   "The main entry point into octopress-mode."
   (interactive)
@@ -236,6 +340,14 @@ The following keys are available in `octopress-mode':
   "The major mode for interacting with a Jekyll server process.
 
 The following keys are available in `octopress-server-mode':
+
+  \\{octopress-server-mode-map}"
+  (setq truncate-lines t))
+
+(define-derived-mode octopress-process-mode nil "Octopress[Process]"
+  "The major mode for interacting with Octopress and Jekyll shell commands.
+
+The following keys are available in `octopress-process-mode':
 
   \\{octopress-server-mode-map}"
   (setq truncate-lines t))
