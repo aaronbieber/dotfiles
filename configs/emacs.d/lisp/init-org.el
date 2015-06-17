fe36806dba05e08f4913673154b84ac5f422b214
Aaron Bieber <aaron@aaronbieber.com>
2015-06-17 10:02:05 -0400

    Better org stuff, in its own file.

---
+++ configs/emacs.d/lisp/init-org.el
@@ -0,0 +1,21 @@
+(when (and (maybe-require-package 'org)
+           (maybe-require-package 'evil-leader))
+
+  (evil-leader/set-key-for-mode 'org-mode
+    "t"  'org-set-tags
+    "p"  '(lambda ()
+            (interactive)
+            (org-insert-property-drawer))
+    "d"  'org-deadline
+    "s"  'org-schedule
+    "a"  'org-agenda
+    "ns" 'org-narrow-to-subtree
+    "$"  'org-archive-subtree)
+
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (evil-define-key 'normal org-mode-map (kbd "TAB") 'org-cycle)
+              (evil-define-key 'normal org-mode-map (kbd "C-\\") 'org-insert-heading)
+              (evil-define-key 'insert org-mode-map (kbd "C-\\") 'org-insert-heading)
+              (auto-fill-mode)
+              (flyspell-mode))))
