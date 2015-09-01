8b6699b831cc2a1543257763a59f44a161cb7c3a
Aaron Bieber <aaron@aaronbieber.com>
2015-09-01 09:12:46 -0400

    Remove unnecessary default font declaration

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -358,7 +358,6 @@ is the buffer location at which the function was found."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#161A1F" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 117 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(comint-highlight-prompt ((t nil)))
  '(powerline-evil-normal-face ((t (:inherit powerline-evil-base-face :background "green" :foreground "black"))))
  '(term-color-blue ((t (:background "DodgerBlue2" :foreground "DodgerBlue2"))))
