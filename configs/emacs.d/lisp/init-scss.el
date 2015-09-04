df7766bcf9beb721f270210aa2eb9cae21c73245
Aaron Bieber <aaron@aaronbieber.com>
2015-09-04 12:53:52 -0400

    Refactor single-line function into lambda

--- configs/emacs.d/lisp/init-scss.el
+++ configs/emacs.d/lisp/init-scss.el
@@ -1,7 +1,5 @@
 (when (maybe-require-package 'scss-mode)
-  (defun scss-settings ()
-    (setq-default css-indent-offset 2))
-
-  (add-hook 'scss-mode-hook 'scss-settings))
+  (add-hook 'scss-mode-hook (lambda ()
+                              (setq-default css-indent-offset 2)))
 
 (provide 'init-scss)
