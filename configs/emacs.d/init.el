afe587b2122459079b83d0c5533e84e0b4fc7292
Aaron Bieber <aaron@aaronbieber.com>
2015-09-10 15:35:42 -0700

    Again remove package-initialize call I don't want

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -6,12 +6,7 @@
 ;;
 ;;; Code:
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;(package-initialize)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
