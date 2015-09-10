41290bb4d45b0564fe382f7dc0a52f0aa6748fb5
Aaron Bieber <aaron@aaronbieber.com>
2015-09-10 07:50:31 -0700

    Update 'om' Ex command to call correct elisp func

--- configs/emacs.d/lisp/init-evil.el
+++ configs/emacs.d/lisp/init-evil.el
@@ -131,6 +131,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
   ;; My own Ex commands.
-  (evil-ex-define-cmd "om" 'om-status))
+  (evil-ex-define-cmd "om" 'octopress-status))
 
 (provide 'init-evil)
