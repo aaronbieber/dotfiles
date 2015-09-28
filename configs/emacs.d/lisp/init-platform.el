0a2bdb349c8ce82a31f7e83c1e184cdf5bf0d088
Aaron Bieber <aaron@aaronbieber.com>
2015-09-28 08:19:29 -0400

    Configure fonts after window setup, so it works

--- configs/emacs.d/lisp/init-platform.el
+++ configs/emacs.d/lisp/init-platform.el
@@ -1,15 +1,22 @@
-;;; Provide settings for specific platforms.
+;;; init-platform --- Platform-specific settings
+;;; Commentary:
+
+;;; Code:
 (require 'init-fonts)
 
-(when (memq window-system '(mac ns))
-  (add-to-list 'default-frame-alist '(font . "Menlo-14"))
-  (set-face-attribute 'default t :font "Menlo-14")
-  (sanityinc/set-frame-font-size 14)
-  (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
+;; This must run after window setup or it seems to have no effect.
+(add-hook 'window-setup-hook
+          (lambda ()
+            (when (memq window-system '(mac ns))
+              (add-to-list 'default-frame-alist '(font . "Menlo-14"))
+              (set-face-attribute 'default t :font "Menlo-14")
+              (sanityinc/set-frame-font-size 14)
+              (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
-(when (memq window-system '(x))
-  (add-to-list 'default-frame-alist '(font . "Hack"))
-  (set-face-attribute 'default t :font "Hack")
-  (sanityinc/set-frame-font-size 10))
+            (when (memq window-system '(x))
+              (add-to-list 'default-frame-alist '(font . "Hack"))
+              (set-face-attribute 'default t :font "Hack")
+              (sanityinc/set-frame-font-size 16))))
 
 (provide 'init-platform)
+;;; init-platform.el ends here
