ab53ebb4f47a0fc54b317ebb69b918893b7c8fbb
Aaron Bieber <aaron@aaronbieber.com>
2015-11-01 13:14:16 -0500

    Use Hack on Macs now

--- configs/emacs.d/lisp/init-platform.el
+++ configs/emacs.d/lisp/init-platform.el
@@ -8,14 +8,14 @@
 (add-hook 'window-setup-hook
           (lambda ()
             (when (memq window-system '(mac ns))
-              (add-to-list 'default-frame-alist '(font . "Menlo-14"))
-              (set-face-attribute 'default t :font "Menlo-14")
+              (add-to-list 'default-frame-alist '(font . "Hack"))
+              (set-face-attribute 'default nil :font "Hack")
               (sanityinc/set-frame-font-size 14)
               (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
             (when (memq window-system '(x))
               (add-to-list 'default-frame-alist '(font . "Hack"))
-              (set-face-attribute 'default t :font "Hack")
+              (set-face-attribute 'default nil :font "Hack")
               (sanityinc/set-frame-font-size 16))))
 
 (provide 'init-platform)
