05a8d89ad42477ec9cc1869a162dc2843c62350a
Aaron Bieber <aaron@aaronbieber.com>
2015-11-09 08:31:54 -0500

    Use bpr to run GTAGS generation + docs

--- configs/emacs.d/lisp/init-gtags.el
+++ configs/emacs.d/lisp/init-gtags.el
@@ -1,4 +1,8 @@
-(when (maybe-require-package 'gtags)
+;;; init-gtags.el -- Configuration for GNU Global
+;;; Commentary:
+;;; Code:
+(when (and (maybe-require-package 'gtags)
+           (maybe-require-package 'bpr))
   ;; Bind some useful keys in the gtags select buffer that evil overrides.
   (add-hook 'gtags-select-mode-hook
             (lambda ()
@@ -8,28 +12,16 @@
   (defun gtags-reindex ()
     "Kick off gtags reindexing."
     (interactive)
-    (let* ((root-path (vc-find-root (buffer-file-name) ".git"))
+    (let* ((root-path (expand-file-name (vc-git-root (buffer-file-name))))
            (gtags-path (concat root-path "GTAGS")))
       (gtags-reindex-process root-path)))
 
   (defun gtags-reindex-process (path)
     "Internal function triggered by gtags-reindex that recursively generates GTAGS."
-    (let ((gtags-buffer (get-buffer-create "*gtags: reindex*")))
-      (with-current-buffer gtags-buffer
-        (kill-all-local-variables)
-        (setq buffer-read-only t)
-        (let ((inhibit-read-only t) (erase-buffer)))
-        (display-buffer (current-buffer))
-        ;; @TODO make this not PHP specific.
-        (let ((cmd (concat "cd " (shell-quote-argument path) " && "
-                           "find . -type f -iname '*php' | "
-                           "gtags -v -f - &"))
-              (map (make-sparse-keymap)))
-          ;;(set-keymap-parent map (current-local-map))
-          (define-key map "q" #'(lambda () (interactive) (kill-buffer (current-buffer))))
-          (use-local-map map)
-          (shell-command cmd (current-buffer))
-          (if (fboundp 'evil-normal-state)
-              (evil-normal-state)))))))
+    (let ((cmd (concat "cd " (shell-quote-argument path) " && "
+                       "find . -type f -iname '*php' | "
+                       "gtags -v -f -")))
+    (bpr-spawn cmd))))
 
 (provide 'init-gtags)
+;;; init-gtags.el ends here
