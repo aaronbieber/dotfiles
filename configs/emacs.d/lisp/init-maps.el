3f6232be9402be4b4ef817c5353eac92ae3ea72f
Aaron Bieber <aaron@aaronbieber.com>
2016-01-05 11:07:31 -0500

    Improved "edit some org file" maps

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -1,21 +1,32 @@
+;;; init-maps.el -- Provide global key maps
+
+;;; Commentary:
 ;;; Provide global maps that aren't specific to any mode or package.
 
-(defun air--pop-to-file (path)
-  (find-file-other-window path))
+;;; Code:
+(defun air--pop-to-file (file &optional split)
+  "Visit a FILE, either in the current window or a SPLIT."
+  (if split
+      (find-file-other-window file)
+    (find-file file)))
 
-(defun air-pop-to-org-todo ()
-  (interactive)
-  (air--pop-to-file "~/Dropbox/org/todo.org"))
+(defun air-pop-to-org-todo (split)
+  "Visit my main TODO list, in the current window or a SPLIT."
+  (interactive "P")
+  (air--pop-to-file "~/Dropbox/org/todo.org" split))
 
-(defun air-pop-to-org-notes ()
-  (interactive)
-  (air--pop-to-file "~/Dropbox/org/notes.org"))
+(defun air-pop-to-org-notes (split)
+  "Visit my main notes file, in the current window or a SPLIT."
+  (interactive "P")
+  (air--pop-to-file "~/Dropbox/org/notes.org" split))
 
-(defun air-pop-to-org-vault ()
-  (interactive)
-  (air--pop-to-file "~/Dropbox/org/vault.gpg"))
+(defun air-pop-to-org-vault (split)
+  "Visit my encrypted vault file, in the current window or a SPLIT."
+  (interactive "P")
+  (air--pop-to-file "~/Dropbox/org/vault.gpg" split))
 
 (defun air-pop-to-org-agenda (split)
+  "Visit the org agenda, in the current window or a SPLIT."
   (interactive "P")
   (org-agenda-list)
   (when (not split)
@@ -36,3 +47,4 @@
   (evil-define-key 'insert global-map (kbd "C-v") 'yank))
 
 (provide 'init-maps)
+;;; init-maps.el ends here
