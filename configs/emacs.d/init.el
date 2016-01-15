9232450f03dc0fd69fe57582df2ccabdb7adc5c9
Aaron Bieber <aaron@aaronbieber.com>
2016-01-15 06:08:27 -0500

    Move company settings from custom set variables

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -118,6 +118,8 @@
   :init
   (global-company-mode)
   :config
+  (setq company-idle-delay 0.5)
+  (setq company-selection-wrap-around t)
   (define-key company-active-map [tab] 'company-complete)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
