aa54f9b69980448cab469b053d8bbb6b2d156f5e
Aaron Bieber <aaron@aaronbieber.com>
2016-01-29 12:20:31 -0500

    Eval buffer mapping

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -8,6 +8,7 @@
 (define-key global-map (kbd "C-c C-u") 'insert-char) ;; "u" for Unicode, get it?
 (define-key global-map (kbd "C-c l")   'dictionary-lookup-definition)
 (define-key global-map (kbd "C-c d f") 'find-name-dired)
+(define-key global-map (kbd "s-e") 'eval-buffer)
 
 ;; C-v is "visual block" in normal mode, but use it for "paste" in insert mode.
 (when (equal system-type 'darwin)
