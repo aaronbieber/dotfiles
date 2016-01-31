185d6682bfb8ba9aa9b617871422874a65cb0eb3
Aaron Bieber <aaron@aaronbieber.com>
2016-01-31 17:13:08 -0500

    Documentation and tidying

--- configs/slate.js
+++ configs/slate.js
@@ -1,7 +1,9 @@
 // My Slate configuration
 
+// Store current window states.
 var windowStates = {};
 
+// Left screen edge operations.
 var leftBarThird = S.op("push",
                         {"direction": "left",
                          "style": "bar-resize:screenSizeX/3"});
@@ -12,6 +14,7 @@ var leftBarTwoThirds = S.op("push",
                             {"direction": "left",
                              "style": "bar-resize:screenSizeX/3*2"});
 
+// Right screen edge operations.
 var rightBarThird = S.op("push",
                          {"direction": "right",
                           "style": "bar-resize:screenSizeX/3"});
@@ -22,9 +25,14 @@ var rightBarTwoThirds = S.op("push",
                              {"direction": "right",
                               "style": "bar-resize:screenSizeX/3*2"});
 
+/* This closure returns a function that will cycle a window through
+ * the states given, assuming that each state is the string name of a
+ * valid Slate operation object created with `S.op()'.
+ *
+ * The function returned is expected to be bound to a key and thus
+ * accepts one argument, the window object.
+ */
 function getCycleStates(states) {
-    var states = states;
-
     return function(win) {
         if (win.pid in windowStates &&
             windowStates[win.pid] !== undefined &&
@@ -40,18 +48,25 @@ function getCycleStates(states) {
         eval(windowStates[win.pid] + '.run()');
     };
 }
+
+// Window cycling functions.
 var leftCycleOp = getCycleStates(['leftBarHalf', 'leftBarThird', 'leftBarTwoThirds']);
 var rightCycleOp = getCycleStates(['rightBarHalf', 'rightBarThird', 'rightBarTwoThirds']);
 
-S.bind('h:cmd', leftCycleOp);
-S.bind('l:cmd', rightCycleOp);
+// Bind the window cycling functions.
+S.bind('h:cmd,shift', leftCycleOp);
+S.bind('l:cmd,shift', rightCycleOp);
 
-S.bind('f:ctrl,shift',
-       S.op('move',
-            {
-                'x': 0,
-                'y': 0,
-                'width': 'screenSizeX',
-                'height': 'screenSizeY'
-            })
-      );
+// Full-screen.
+S.bind(
+    'f:cmd,shift',
+    S.op(
+        'move',
+        {
+            'x': 0,
+            'y': 0,
+            'width': 'screenSizeX',
+            'height': 'screenSizeY'
+        }
+    )
+);
