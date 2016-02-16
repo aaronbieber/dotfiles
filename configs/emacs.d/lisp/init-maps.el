290a3710112366e6d27c7113866b16cfa5da93fc
Aaron Bieber <aaron@aaronbieber.com>
2016-02-15 19:14:36 -0500

    Key map for opening the terminal emulator

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -6,8 +6,9 @@
 ;;; Code:
 (define-key global-map (kbd "C-x C-q") 'kill-emacs)
 (define-key global-map (kbd "C-c C-u") 'insert-char) ;; "u" for Unicode, get it?
-(define-key global-map (kbd "C-c l")   'dictionary-lookup-definition)
 (define-key global-map (kbd "C-c d f") 'find-name-dired)
+(define-key global-map (kbd "C-c l")   'dictionary-lookup-definition)
+(define-key global-map (kbd "C-c s")   (lambda () (interactive) (ansi-term "zsh")))
 (define-key global-map (kbd "s-e") 'eval-buffer)
 (define-key global-map (kbd "s-d") 'eval-defun)
 (define-key global-map (kbd "C-}") 'air-cycle-theme)
