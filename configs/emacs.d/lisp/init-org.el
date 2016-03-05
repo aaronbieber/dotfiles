ebd6453096111e893b1f4b5c06dc80ad525fbd33
Aaron Bieber <aaron@aaronbieber.com>
2016-03-05 15:47:35 -0500

    Shortcut for running org-agenda

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -13,6 +13,7 @@
          ("C-c t t" . air-pop-to-org-todo)
          ("C-c t v" . air-pop-to-org-vault)
          ("C-c t a" . air-pop-to-org-agenda)
+         ("C-c t A" . org-agenda)
          ("C-c f k" . org-search-view)
          ("C-c f t" . org-tags-view))
   :config
