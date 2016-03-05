8a4ba1eb4a427144db81b1504cadfd8e35f5a3aa
Aaron Bieber <aaron@aaronbieber.com>
2016-03-05 16:09:59 -0500

    Better tag handling commands

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -89,21 +89,28 @@ SCHEDULED: %t")))
         (insert (concat " " char " "))
       (insert char)))
 
-  (defun air--org-swap-tags (old-tags new-tags)
-    "Swap OLD-TAGS for NEW-TAGS and re-indent."
+  (defun air--org-swap-tags (tags)
+    "Replace any tags on the current headline with TAGS.
+
+The assumption is that TAGS will be a string conforming to Org Mode's
+tag format specifications, or nil to remove all tags."
     (interactive)
-    (let ((old-tags (if old-tags old-tags (org-get-tags-string)))
-          (new-tags (concat " " new-tags)))
+    (let ((old-tags (org-get-tags-string))
+          (tags (if tags
+                    (concat " " tags)
+                  "")))
       (save-excursion
         (beginning-of-line)
         (re-search-forward
          (concat "[ \t]*" (regexp-quote old-tags) "[ \t]*$")
          (line-end-position) t)
-        (replace-match new-tags)
+        (replace-match tags)
         (org-set-tags t))))
 
-  (defun air-org-set-tag (tag)
-    "Add TAG, chosen interactively, to the current headline."
+  (defun air-org-set-tags (tag)
+    "Add TAG if it is not in the list of tags, remove it otherwise.
+
+TAG is chosen interactively from the global tags completion table."
     (interactive
      (list (let ((org-last-tags-completion-table
                   (if (derived-mode-p 'org-mode)
@@ -114,22 +121,15 @@ SCHEDULED: %t")))
              (org-icompleting-read
               "Tag: " 'org-tags-completion-function nil nil nil
               'org-tags-history))))
-    (let* ((cur-string (org-get-tags-string))
-           (cur-list (org-get-tags))
-           (new (concat " :" (mapconcat 'identity (append cur-list (list tag)) ":") ":")))
-      (air--org-swap-tags cur-string new)))
-
-  (defun air-org-remove-tag (tag)
-    "Remove TAG, chosen interactively, from the current headline."
-    (interactive
-     (list (org-icompleting-read
-            "Tag: " (org-get-tags))))
-
-    (if tag
-        (let* ((new (concat ":" (mapconcat
-                                 'identity (delete tag (org-get-tags)) ":") ":")))
-          (air--org-swap-tags nil new))))
-
+    (let* ((cur-list (org-get-tags))
+           (new-tags (mapconcat 'identity
+                                (if (member tag cur-list)
+                                    (delete tag cur-list)
+                                  (append cur-list (list tag)))
+                                ":"))
+           (new (if (> (length new-tags) 1) (concat " :" new-tags ":")
+                  nil)))
+      (air--org-swap-tags new)))
 
   (add-hook 'org-agenda-mode-hook
             (lambda ()
