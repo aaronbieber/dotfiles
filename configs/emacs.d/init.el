8a592146e697deb298e34ec9c0f55592c2cf7e1c
Aaron Bieber <aaron@aaronbieber.com>
2016-03-07 10:39:28 -0500

    Custom colors that I don't recall setting

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -435,7 +435,7 @@ is the buffer location at which the function was found."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background "#282828" :foreground "#fdf4c1")))))
 
 ;; handle tmux's xterm-keys
 ;; put the following line in your ~/.tmux.conf:
