7feaf7c95c54d6d202481c8f1e6c33e54677b6f4
Aaron Bieber <aaron@aaronbieber.com>
2016-03-09 10:17:59 -0500

    Kill dumb, old maps

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -6,14 +6,13 @@
 ;;; Code:
 (define-key global-map (kbd "C-x C-q") 'kill-emacs)
 (define-key global-map (kbd "C-c C-u") 'insert-char) ;; "u" for Unicode, get it?
-(define-key global-map (kbd "C-c d f") 'find-name-dired)
-(define-key global-map (kbd "C-c l")   'dictionary-lookup-definition)
 (define-key global-map (kbd "C-c s")   (lambda () (interactive) (ansi-term "zsh")))
-(define-key global-map (kbd "s-e") 'eval-buffer)
-(define-key global-map (kbd "s-d") 'eval-defun)
-(define-key global-map (kbd "C-}") 'air-cycle-theme)
+(define-key global-map (kbd "s-e")     'eval-buffer)
+(define-key global-map (kbd "s-d")     'eval-defun)
+(define-key global-map (kbd "C-}")     'air-cycle-theme)
 
 ;; C-v is "visual block" in normal mode, but use it for "paste" in insert mode.
+;; Only on Macs, of course.
 (when (equal system-type 'darwin)
   (evil-define-key 'insert global-map (kbd "C-v") 'yank))
 
