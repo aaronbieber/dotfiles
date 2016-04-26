59dfab29f1eb0047a5857c0f06ed63d2da2ec620
Aaron Bieber <aaron@aaronbieber.com>
2016-04-26 06:50:23 -0400

    Better Python mode

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -329,8 +329,32 @@
             (define-key lisp-interaction-mode-map (kbd "<C-return>") 'eval-last-sexp)))
 
 ;;; Python mode:
+
+(use-package virtualenvwrapper
+  :ensure t
+  :config
+  (venv-initialize-interactive-shells)
+  (venv-initialize-eshell)
+  (setq venv-location
+        (expand-file-name "~/Projects/virtualenvs/")))
+
 (add-hook 'python-mode-hook
           (lambda ()
+            ;; I'm rudely redefining this function to do a comparison of `point'
+            ;; to the end marker of the `comint-last-prompt' because the original
+            ;; method of using `looking-back' to match the prompt was never
+            ;; matching, which hangs the shell startup forever.
+            (defun python-shell-accept-process-output (process &optional timeout regexp)
+              "Redefined to actually work."
+              (let ((regexp (or regexp comint-prompt-regexp)))
+                (catch 'found
+                  (while t
+                    (when (not (accept-process-output process timeout))
+                      (throw 'found nil))
+                    (when (= (point) (cdr (python-util-comint-last-prompt)))
+                      (throw 'found t))))))
+
+            ;; Additional settings follow.
             (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
 ;;; Magit mode (which does not open in evil-mode):
