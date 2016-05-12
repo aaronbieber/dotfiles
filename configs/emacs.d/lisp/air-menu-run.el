8a9f9647dacb374c1e4862819fc7351d7c6abe7f
Aaron Bieber <aaron@aaronbieber.com>
2016-05-12 17:12:00 -0400

    Modify air-menu-run to have menu titles

--- configs/emacs.d/lisp/air-menu-run.el
+++ configs/emacs.d/lisp/air-menu-run.el
@@ -17,31 +17,61 @@
 (defvar air-menu-run-items
   '(())
   "An alist of menus.
+
 The keys in the alist are simple strings used to reference the menu in
 calls to `air-menu-run' and the values are lists with three elements:
 A raw character to use as the selection key, such as `?a'; a string to
 use in the menu display, and a function to call when that item is
-selected.")
+selected.
 
-(defun air-menu-run (menu)
-  "Display the items in MENU and run the selected item."
+The data structure should look like:
+
+'((\"menu-1\" (?a \"First item\" function-to-call-for-item-1)
+            (?b \"Second item\" function-to-call-for-item-2))
+  (\"menu-2\" (?z \"First item\" function-to-call-for-item-1)
+            (?x \"Second item\" function-to-call-for-item-2)))")
+
+(defun air-menu-run (&optional menu)
+  "Display the items in MENU and run the selected item.
+
+If MENU is not given, a dynamically generated menu of available menus
+is displayed."
   (interactive)
-  (if (assoc menu air-menu-run-items)
-      (let* ((items (append (cadr (assoc menu air-menu-run-items))
-                            '((?q "Quit" nil))))
-             (prompt (mapconcat (lambda (i)
-                                  (concat
-                                   (propertize (concat
-                                                "[" (char-to-string (nth 0 i)) "] ")
-                                               'face 'air-menu-run-heading-face)
-                                   (nth 1 i)))
-                                items ", "))
-             (choices (mapcar (lambda (i) (nth 0 i)) items))
-             (choice (read-char-choice prompt choices)))
-        (if (and (assoc choice items)
-                 (functionp (nth 2 (assoc choice items))))
-            (funcall (nth 2 (assoc choice items)))
-          (message "Menu aborted.")))))
+  (if (< (length air-menu-run-items) 1)
+      (message "Configure air-menu-run-items first.")
+    (let* ((menu (if (assoc menu air-menu-run-items)
+                     (cadr (assoc menu air-menu-run-items))
+                   (air-menu-of-menus)))
+           (title (car menu))
+           (items (append (cadr menu)
+                          '((?q "Quit" nil))))
+           (prompt (concat title ": "
+                           (mapconcat (lambda (i)
+                                        (concat
+                                         (propertize (concat
+                                                      "[" (char-to-string (nth 0 i)) "] ")
+                                                     'face 'air-menu-run-heading-face)
+                                         (nth 1 i)))
+                                      items ", ")))
+                   (choices (mapcar (lambda (i) (nth 0 i)) items))
+                   (choice (read-char-choice prompt choices)))
+           (if (and (assoc choice items)
+                    (functionp (nth 2 (assoc choice items))))
+               (funcall (nth 2 (assoc choice items)))
+             (message "Menu aborted.")))))
+
+(defun air-menu-of-menus ()
+  "Build menu items for all configured menus.
+
+This allows `air-menu-run' to display an interactive menu of all
+configured menus if the caller does not specify a menu name
+explicitly."
+  (let ((menu-key-char 97))
+    `("Menus" ,(mapcar (lambda (i)
+                (prog1
+                    `(,menu-key-char ,(car i) (lambda () (air-menu-run ,(car i))))
+                  (setq menu-key-char (1+ menu-key-char))))
+              air-menu-run-items))))
 
 (provide 'air-menu-run)
 ;;; air-menu-run ends here
