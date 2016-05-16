b5b7af34c2a92502cb6eeff1d43c4682c4c2893d
Aaron Bieber <aaron@aaronbieber.com>
2016-05-16 09:11:57 -0400

    Prevent visual bell race condition

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -44,18 +44,26 @@
 (put 'narrow-to-region 'disabled nil)
 
 ;; The OS X visible bell is buggy as hell.
+(defvar air-bell-ringing nil
+  "Whether my visual bell is currently being rung.
+
+This prevents simultaneously ringing two bells and falling into a race
+condition where the bell visualization never clears.")
+
 (setq ring-bell-function (lambda ()
-                           (let* ((bg (face-background 'default))
-                                  (fg (face-foreground 'default))
-                                  (reset `(lambda ()
-                                            (set-face-background 'default ,bg)
-                                            (set-face-foreground 'default ,fg))))
+                           (if (not air-bell-ringing)
+                               (let* ((bg (face-background 'default))
+                                      (fg (face-foreground 'default))
+                                      (reset `(lambda ()
+                                                (set-face-background 'default ,bg)
+                                                (set-face-foreground 'default ,fg)
+                                                (setq air-bell-ringing nil))))
 
-                             (set-face-background 'default "gray")
-                             (set-face-foreground 'default "black")
+                                 (set-face-background 'default "NavajoWhite4")
+                                 ;(set-face-foreground 'default "black")
+                                 (setq air-bell-ringing t)
 
-                             (run-with-timer 0.05 nil reset))))
-
+                                 (run-with-timer 0.05 nil reset)))))
 
 (defun my-minibuffer-setup-hook ()
   "Increase GC cons threshold."
