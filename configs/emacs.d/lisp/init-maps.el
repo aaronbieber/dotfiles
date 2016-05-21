0b6714d864e7756bbda7e04a4cbf904b0626f2d2
Aaron Bieber <aaron@aaronbieber.com>
2016-05-21 11:50:38 -0400

    Move eval-defun to evil

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -8,7 +8,6 @@
 (define-key global-map (kbd "C-c u")   'insert-char) ;; "u" for Unicode, get it?
 (define-key global-map (kbd "C-c s")   (lambda () (interactive) (ansi-term "zsh")))
 (define-key global-map (kbd "s-e")     'eval-buffer)
-(define-key global-map (kbd "s-d")     'eval-defun)
 (define-key global-map (kbd "C-}")     'air-cycle-theme)
 
 ;; C-v is "visual block" in normal mode, but use it for "paste" in insert mode.
