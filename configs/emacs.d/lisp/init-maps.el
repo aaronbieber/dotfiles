23a2f73144597016f433d043592dfb07ee1e7b3b
Aaron Bieber <aaron@aaronbieber.com>
2016-09-18 08:58:42 -0400

    I always use Macs now, so, stop checking

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -9,11 +9,7 @@
 (define-key global-map (kbd "C-c s")   (lambda () (interactive) (ansi-term "zsh")))
 (define-key global-map (kbd "s-e")     'eval-buffer)
 (define-key global-map (kbd "C-}")     'air-cycle-theme)
-
-;; C-v is "visual block" in normal mode, but use it for "paste" in insert mode.
-;; Only on Macs, of course.
-(when (equal system-type 'darwin)
-  (evil-define-key 'insert global-map (kbd "C-v") 'yank))
+(evil-define-key 'insert global-map (kbd "C-v") 'yank)
 
 (provide 'init-maps)
 ;;; init-maps.el ends here
