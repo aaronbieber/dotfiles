2956171adc7d54861ca3bba7f4178d8393b80d74
Aaron Bieber <aaron@aaronbieber.com>
2016-09-18 08:58:55 -0400

    Impl. use-package, format to standards

--- configs/emacs.d/lisp/init-twitter.el
+++ configs/emacs.d/lisp/init-twitter.el
@@ -1,7 +1,15 @@
-(when (maybe-require-package 'twittering-mode)
+;;; init-twitter.el -- Tweet from Emacs
+;;; Commentary:
+;;; Code:
+
+(use-package twittering-mode
+  :ensure t
+  :commands (twit twittering-mode)
+  :config
   (add-hook 'twittering-mode-hook
             (lambda ()
               (define-key twittering-mode-map (kbd ",o") 'delete-other-windows)
               (define-key twittering-mode-map (kbd ",b") 'helm-mini))))
 
 (provide 'init-twitter)
+;;; init-twitter.el ends here
