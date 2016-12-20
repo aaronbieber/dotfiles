a1db5c6a3dbe6aabb0cf112f52e750f1498ae15d
Aaron Bieber <aaron@aaronbieber.com>
2016-12-20 10:50:43 -0500

    Add pcomplete to completion at point functions
    This allow completions of #+ to work in Org Mode, which is fairly
    essential for the number of things that can possibly follow #+.

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -102,6 +102,9 @@ condition where the bell visualization never clears.")
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
 
+;;; Use pcomplete because it helps in org-mode
+(add-to-list 'completion-at-point-functions 'pcomplete)
+
 ;;; My own configurations, which are bundled in my dotfiles.
 (require 'init-platform)
 (require 'init-global-functions)
