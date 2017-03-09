08f85441198354e74b5843d8c848ed053319fac5
Aaron Bieber <aaron@aaronbieber.com>
2017-03-08 20:39:25 -0500

    Copy GOPATH from shell environment

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -220,7 +220,8 @@ condition where the bell visualization never clears.")
 (use-package exec-path-from-shell
   :ensure t
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
 
 (use-package elpy
   :ensure t
