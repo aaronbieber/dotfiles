b2d3160b7d898da5ce3ea68dccdeca5a66b51951
Aaron Bieber <aaron@aaronbieber.com>
2017-03-08 20:38:02 -0500

    Remove C-x C-q map so it can do the wdired thing

--- configs/emacs.d/lisp/init-maps.el
+++ configs/emacs.d/lisp/init-maps.el
@@ -4,7 +4,6 @@
 ;;; Provide global maps that aren't specific to any mode or package.
 
 ;;; Code:
-(define-key global-map (kbd "C-x C-q") 'kill-emacs)
 (define-key global-map (kbd "C-c u")   'insert-char) ;; "u" for Unicode, get it?
 (define-key global-map (kbd "C-c s")   (lambda () (interactive) (ansi-term "zsh")))
 (define-key global-map (kbd "s-e")     'eval-buffer)
