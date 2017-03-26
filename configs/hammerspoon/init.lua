2937eb9ee7d4a3880e735dc3a4d916acff69c49e
Aaron Bieber <aaron@aaronbieber.com>
2017-03-26 14:16:56 -0400

    Hammerspoon config now mirrors Slate's

--- configs/hammerspoon/init.lua
+++ configs/hammerspoon/init.lua
@@ -36,6 +36,30 @@ function rightBarTwoThirds(win)
    win:move({0.33, 0, 2/3, 1}, win:screen())
 end
 
+function topHalf(win)
+   local winGeo = win:frame()
+   local screenFrame = win:screen():frame()
+   win:move({winGeo.x, screenFrame.y, winGeo.w, screenFrame.h / 2})
+end
+
+function topThird(win)
+   local winGeo = win:frame()
+   local screenFrame = win:screen():frame()
+   win:move({winGeo.x, screenFrame.y, winGeo.w, screenFrame.h / 3})
+end
+
+function bottomHalf(win)
+   local winGeo = win:frame()
+   local screenFrame = win:screen():frame()
+   win:move({winGeo.x, screenFrame.y + (screenFrame.h / 2), winGeo.w, screenFrame.h / 2})
+end
+
+function bottomThird(win)
+   local winGeo = win:frame()
+   local screenFrame = win:screen():frame()
+   win:move({winGeo.x, screenFrame.y + ((screenFrame.h / 3) * 2), winGeo.w, screenFrame.h / 3})
+end
+
 function indexOf(list, value)
    for index, list_value in pairs(list) do
       if list_value == value then
@@ -64,7 +88,7 @@ function cycleState(states)
    else
       local i = indexOf(states, state)
       log.df("Got win %s current state index of %s", id, i)
-      if i == 3 then
+      if i == #states then
          i = 1
       else
          i = i + 1
@@ -81,6 +105,14 @@ end
 
 function resizeRight()
    cycleState({rightBarHalf, rightBarThird, rightBarTwoThirds})
+end
+
+function resizeUp()
+   cycleState({topHalf, topThird})
+end
+
+function resizeDown()
+   cycleState({bottomHalf, bottomThird})
 end
 
 function moveLeft()
@@ -101,6 +133,8 @@ end
 
 hs.hotkey.bind("cmd", "left", resizeLeft)
 hs.hotkey.bind("cmd", "right", resizeRight)
+hs.hotkey.bind("cmd", "up", resizeUp)
+hs.hotkey.bind("cmd", "down", resizeDown)
 hs.hotkey.bind({"cmd", "shift"}, "right", moveRight)
 hs.hotkey.bind({"cmd", "shift"}, "left", moveLeft)
 hs.hotkey.bind({"cmd", "shift"}, "up", fullScreen)
