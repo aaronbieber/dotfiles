4473cd77598193a5e75816bc0668476402a8c017
Aaron Bieber <aaron@aaronbieber.com>
2017-08-06 06:41:11 -0400

    Easier wdired mode
    Enter wdired mode with `C-c C-c`, and always begin in Evil normal
    state.

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -231,12 +231,16 @@ condition where the bell visualization never clears.")
   (add-hook 'dired-mode-hook (lambda ()
                                (dired-omit-mode t)
                                (all-the-icons-dired-mode t)))
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
-  (define-key dired-mode-map (kbd "^")   (lambda () (interactive) (find-alternate-file "..")))
-  (define-key dired-mode-map (kbd "C-.") 'dired-omit-mode)
-  (define-key dired-mode-map (kbd "c")   'find-file)
-  (define-key dired-mode-map (kbd "/")   'counsel-grep-or-swiper)
-  (define-key dired-mode-map (kbd "?")   'evil-search-backward))
+  (define-key dired-mode-map (kbd "RET")     'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "^")       (lambda () (interactive) (find-alternate-file "..")))
+  (define-key dired-mode-map (kbd "C-.")     'dired-omit-mode)
+  (define-key dired-mode-map (kbd "c")       'find-file)
+  (define-key dired-mode-map (kbd "/")       'counsel-grep-or-swiper)
+  (define-key dired-mode-map (kbd "?")       'evil-search-backward)
+  (define-key dired-mode-map (kbd "C-c C-c") 'dired-toggle-read-only))
+
+(eval-after-load 'wdired
+  (add-hook 'wdired-mode-hook 'evil-normal-state))
 
 (use-package exec-path-from-shell
   :ensure t
