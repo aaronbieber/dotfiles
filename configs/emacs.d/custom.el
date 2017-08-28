db894af51af06f15c4183592fc3b8f73081a870a
Aaron Bieber <aaron@aaronbieber.com>
2017-08-28 07:40:50 -0400

    Magit commit w/default args

--- configs/emacs.d/custom.el
+++ configs/emacs.d/custom.el
@@ -12,6 +12,7 @@
    (quote
     ("91ce1e1533921a31778b95d1626371499e42d46833d703e25a07dcb8c283f4af" . t)))
  '(fci-rule-color "#d6d6d6")
+ '(magit-commit-arguments nil)
  '(markdown-preview-custom-template "/Users/abieber/.emacs.d/markdown-preview-template.html")
  '(org-hide-emphasis-markers t)
  '(org-modules
@@ -27,6 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 16777215)) (:background "#282828" :foreground "#fdf4c1")) (((class color) (min-colors 255)) (:background "#262626" :foreground "#ffffaf"))))
  '(term ((t (:foreground "ivory1"))))
  '(term-color-black ((t (:foreground "gray80"))))
  '(term-color-cyan ((t (:foreground "cyan2"))))
