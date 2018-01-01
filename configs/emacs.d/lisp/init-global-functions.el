024317d6060cde7776cb8f60499e39937bc3604a
Aaron Bieber <aaron@aaronbieber.com>
2018-01-01 08:13:59 -0500

    Much improved "launch an eshell" function

--- configs/emacs.d/lisp/init-global-functions.el
+++ configs/emacs.d/lisp/init-global-functions.el
@@ -32,14 +32,22 @@ with percentage."
       (find-file-other-window file)
     (find-file file)))
 
-(defun air-open-eshell (&optional same-window)
-  "Start EShell, in a split window unless SAME-WINDOW is not nil."
-  (interactive "P")
-  (if same-window
-      (eshell)
-    (let ((buf (eshell)))
-      (switch-to-buffer (other-buffer buf))
-      (switch-to-buffer-other-window buf))))
+(defun air-open-eshell (arg)
+  "Start the Emacs shell.
+
+With one prefix argument ARG, start the shell in the current window.
+With two prefix arguments, force the creation of a new session.
+With three prefix arguments, create a new session in the current window."
+  (interactive "p")
+  (let ((same-window (or (= arg 4)
+                         (= arg 64)))
+        (new-session (or (= arg 16)
+                         (= arg 64))))
+    (if same-window
+        (eshell new-session)
+      (let ((buf (eshell new-session)))
+        (switch-to-buffer (other-buffer buf))
+        (switch-to-buffer-other-window buf)))))
 
 (require 'htmlfontify)
 (defun fontify-and-browse ()
