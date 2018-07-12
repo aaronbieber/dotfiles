4ad5a8f40a09e5e40f24d040484486429efc8b4a
Aaron Bieber <abieber@hubspot.com>
2018-07-12 11:51:47 -0400

    Set default JS indent to 2 spaces

--- configs/emacs.d/custom.el
+++ configs/emacs.d/custom.el
@@ -12,6 +12,7 @@
     ("91ce1e1533921a31778b95d1626371499e42d46833d703e25a07dcb8c283f4af" . t)))
  '(fci-rule-color "#d6d6d6")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(js-indent-level 2)
  '(magit-commit-arguments nil)
  '(markdown-preview-custom-template "/Users/abieber/.emacs.d/markdown-preview-template.html")
  '(org-hide-emphasis-markers t)
@@ -28,4 +29,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 16777215)) (:background "#282828" :foreground "#fdf4c1")) (((class color) (min-colors 255)) (:background "#262626" :foreground "#ffffaf"))))
  '(term ((t (:foreground "ivory1")))))
