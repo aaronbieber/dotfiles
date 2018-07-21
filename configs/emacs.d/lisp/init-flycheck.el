f1fce0b2d79ae7c08fe0c9d060b4b12008e6abd8
Aaron Bieber <aaron@aaronbieber.com>
2018-07-21 06:30:52 -0400

    Strip out unused style standards

--- configs/emacs.d/lisp/init-flycheck.el
+++ configs/emacs.d/lisp/init-flycheck.el
@@ -22,8 +22,7 @@
   (setq flycheck-emacs-lisp-load-path 'inherit
         flycheck-check-syntax-automatically '(save idle-change mode-enabled)
         flycheck-idle-change-delay 0.8
-        flycheck-disabled-checkers '(php-phpmd)
-        flycheck-phpcs-standard "CSNStores")
+        flycheck-disabled-checkers '(php-phpmd))
 
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
