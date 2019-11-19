24a06bb76de223ee09427883fea9ff0a3d2bb328
Aaron Bieber <abieber@hubspot.com>
2019-11-19 14:30:53 -0500

    Use writeroom instead of olivetti for focused writing

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -845,13 +845,13 @@ TAG is chosen interactively from the global tags completion table."
     (if (not (eq major-mode 'org-mode))
         (error "Narrow to prose only works in Org Mode"))
     (if (buffer-narrowed-p)
-        (progn (olivetti-mode 0)
+        (progn (writeroom-mode 0)
                (widen)
                (if (member 'visual-fill-column-mode minor-mode-list)
                   (visual-fill-column-adjust)))
       (let ((bounds (air--org-inner-element-bounds 0)))
         (narrow-to-region (cadr bounds) (car bounds))
-        (olivetti-mode t))))
+        (writeroom-mode t))))
 
   (evil-leader/set-key-for-mode 'org-mode
     "$"  'org-archive-subtree
