bfa099a287b46494ffeca088327f1125669d2e6e
Aaron Bieber <aaron@aaronbieber.com>
2020-01-01 13:58:10 -0500

    Remove unnecessary yasnippet compiled file
    This shouldn't have been version controlled anyway.

--- configs/emacs.d/snippets/markdown-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `markdown-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'markdown-mode
-                     '(("infobox" "{% infobox %}\n$0\n{% endinfobox %}" "The Octopress Infobox" nil nil nil "/Users/abieber/.emacs.d/snippets/markdown-mode/infobox" "direct-keybinding" nil)))
-
-
-;;; Do not edit! File generated at Fri May 27 09:19:49 2016
