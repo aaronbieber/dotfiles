d21a661cd9d25bc1909da631bee3e9f154c6ed4b
Aaron Bieber <aaron@aaronbieber.com>
2020-06-26 10:54:14 -0400

    Cull unused functions

--- configs/emacs.d/lisp/init-global-functions.el
+++ configs/emacs.d/lisp/init-global-functions.el
@@ -230,17 +230,6 @@ Attempts to follow the Do What I Mean philosophy."
       (widen)
     (narrow-to-region-or-subtree)))
 
-(defun open-current-line-in-codebase-search ()
-  "Go to the current file's current line on the codebase site."
-  (interactive)
-  (let* ((line-num (number-to-string (line-number-at-pos)))
-         (file-path (replace-regexp-in-string
-                     (expand-file-name (vc-find-root (buffer-file-name) ".git"))
-                     ""
-                     (buffer-file-name)))
-         (args (concat "http://codesearch.csnzoo.com/source/xref/php/" file-path "#" line-num)))
-    (call-process "open" nil nil nil args)))
-
 ;;; From http://beatofthegeek.com/2014/02/my-setup-for-using-emacs-as-web-browser.html
 (defun wikipedia-search (search-term)
   "Search for SEARCH-TERM on wikipedia"
