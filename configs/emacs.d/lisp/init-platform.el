f77054713a7c046a326b34f525071deb60dc1013
Aaron Bieber <aaron@aaronbieber.com>
2020-09-12 06:46:54 -0400

    Configure proper browser function for WSL/WSL2
    This patch also removes apparently unnecessary `default-frame-alist`
    font settings in favor of the simpler `set-face-attribute`.

--- configs/emacs.d/lisp/init-platform.el
+++ configs/emacs.d/lisp/init-platform.el
@@ -13,23 +13,28 @@
                 :config
                 (exec-path-from-shell-initialize)
                 (exec-path-from-shell-copy-env "GOPATH"))
-              ;;(add-to-list 'default-frame-alist '(font . "Input"))
               (set-face-attribute 'default nil :font "Input" :weight 'light)
               (sanityinc/set-frame-font-size 14)
               (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen))
 
             (when (memq window-system '(w32))
-              ;;(add-to-list 'default-frame-alist '(font . "Hack"))
               (set-face-attribute 'default nil :font "Hack")
               (setq epg-gpg-home-directory "c:/Users/Aaron/AppData/Roaming/GnuPG")
               (setq epg-gpg-program "c:/Users/Aaron/Programs/GnuPG/bin/gpg.exe")
               (setq epg-gpgconf-program "c:/Users/Aaron/Programs/GnuPG/bin/gpgconf.exe")
               (sanityinc/set-frame-font-size 20))
 
-            (when (memq window-system '(x))
-              ;;(add-to-list 'default-frame-alist '(font . "JetBrains Mono"))
+            ;; WSL/WSL2
+            (when (and (eq system-type 'gnu/linux)
+                       (string-match
+                        "Linux.*microsoft.*Linux"
+                        (shell-command-to-string "uname -a")))
               (set-face-attribute 'default nil :font "JetBrains Mono")
-              (sanityinc/set-frame-font-size 16))
+              (sanityinc/set-frame-font-size 16)
+
+              (setq browse-url-generic-program "/c/Windows/system32/cmd.exe"
+                    browse-url-generic-args '("/c" "start")
+                    browse-url-browser-function #'browse-url-generic))
 
             (when (fboundp 'powerline-reset)
               (powerline-reset))))
