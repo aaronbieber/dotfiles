8e4e6d29c1a4aa15ee9be00f78b0f41784ca3ed7
Aaron Bieber <aaron@aaronbieber.com>
2020-09-29 14:08:48 -0400

    Don't show for-meetings items in the date agenda
    For-meetings items with scheduled dates should just appear in the
    for-meetings section of the agenda on that day.

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -984,6 +984,7 @@ the current timestamp."
                      (org-agenda-hide-tags-regexp "active")
                      (org-agenda-prefix-format "%(air--fixed-project-prefix)%?-12t% s")
                      (org-agenda-skip-function '(or (org-agenda-skip-entry-if 'todo '("WAITING"))
+                                                    (air-org-skip-tag-prefix "@")
                                                     (air-org-skip-if-habit)))))
             (tags-todo "active-reading/!-WAITING-DONE-CANCELED"
                        ((org-agenda-overriding-header (air--org-separating-heading "Tasks"))
