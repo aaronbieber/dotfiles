5beebcbfe723e93cf408f9cb61ef7a2d51b438c3
Aaron Bieber <aaron@aaronbieber.com>
2023-05-07 16:01:07 -0400

    Appease the bytecode compiler
    I don't know if this actually worked the way it was before because I
    don't use web-mode much. But nevertheless.

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -521,7 +521,7 @@ goal is to have a blank line between list items."
               (flycheck-mode)))
 
   (add-hook 'web-mode-before-auto-complete-hooks
-            '(lambda ()
+            (lambda ()
                (let ((web-mode-cur-language (web-mode-language-at-pos)))
                  (if (string= web-mode-cur-language "php")
                      (yas-activate-extra-mode 'php-mode)
