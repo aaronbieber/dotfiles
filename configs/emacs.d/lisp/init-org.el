ada222210cde4500f6b8beb432017803ecf05f98
Aaron Bieber <aaron@aaronbieber.com>
2023-05-10 20:29:07 -0400

    No need to specify modules

--- configs/emacs.d/lisp/init-org.el
+++ configs/emacs.d/lisp/init-org.el
@@ -669,8 +669,6 @@ TAG is chosen interactively from the global tags completion table."
   :config
   (require 'org-protocol)
   (setq org-hide-emphasis-markers t)
-  (setq org-modules
-        '(org-bbdb org-bibtex org-docview org-habit org-info org-w3m))
   (setq org-todo-keywords
         '((sequence "TODO(t)" "DOING(o)" "LATER(l)" "WAITING(w!)" "|" "DONE(d!)" "CANCELED(c!)")))
   (setq org-blank-before-new-entry '((heading . t)
